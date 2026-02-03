5f88a31649f5cf6a77118cc3e1e510815046efef
Piyush Tiwari <sirpiyush777@gmail.com>
2026-02-03 21:45:02 +0530

    Add contract schemas, safe write flows, and release gate execution to API framework

--- src/test/java/specs/RequestSpecFactory.java
+++ src/test/java/specs/RequestSpecFactory.java
@@ -1,5 +1,8 @@
 package specs;
 
+import org.mozilla.javascript.ast.ConditionalExpression;
+
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
@@ -24,6 +27,7 @@ public class RequestSpecFactory {
 				.addHeader("Authorization","Bearer " + TokenManager.getToken())
 				.log(LogDetail.URI)
 				.log(LogDetail.METHOD)
+				.addFilter(new AllureRestAssured())
 				.build();
 	}
 }
